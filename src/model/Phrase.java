8811f76bd29bd1436109d652a5a6079babc65cfe
Gilang Julian <gilang.9h@gmail.com>
2016-03-02 23:40:34 +0700

    Initial Commit

---
+++ src/model/Phrase.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author macair
+ */
+public class Phrase {
+    
+    public List<Phrase> childs;
+    public String phrase;
+    public String tag;
+    
+    public Phrase(String phrase, String tag, boolean doProcess){
+        this.phrase = phrase;
+        this.tag = tag;
+        childs = new ArrayList<>();
+        if(doProcess){
+            if(containTag(this.phrase)){
+                process(this.phrase, 0);
+            }else{
+                this.phrase = this.phrase.substring(1, this.phrase.length()-1);
+            }
+        }
+    }
+    
+    public void process(String str, int startIdx){
+        int idx1 = startIdx;
+        int idx2 = idx1;
+        while(idx2 < str.length()-1){
+            idx1 = str.indexOf("(", idx2) + 1;
+            idx2 = str.indexOf(" ", idx1);
+            if(idx1 < 0 || idx2 < 0){
+//                System.out.println("idx1 = " + idx1 + " idx2 = " + idx2);
+                break;
+            }
+            String tag = str.substring(idx1, idx2);
+            idx1 = idx2+1;
+            idx2 = getEndidx(str, idx1);
+            if(idx1 > 0 && idx2 > 0){
+                String childPhrase = str.substring(idx1, idx2);
+                childs.add(new Phrase(childPhrase, tag, true));
+//                System.out.println("tag = " + tag + "\nphrase = " + childPhrase);
+            }else{
+//                System.out.println("idx1 = " + idx1 + " idx2 = " + idx2);
+                break;
+            }
+        }
+    }
+    
+    public boolean containTag(String str){
+        if(str.indexOf('(') != str.lastIndexOf('(')){
+            return true;
+        }
+        return false;
+    }
+    
+    public int getEndidx(String str, int curIdx){
+        int counter = 1;
+        int idx = curIdx;
+        while(counter > 0){
+            if(str.charAt(idx) == '(')
+                counter++;
+            else if(str.charAt(idx) == ')')
+                counter--;
+            idx++;
+        }
+        return idx-1;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+//        ---- parent tag ----
+        if(childs.size() == 0){
+            if(phrase.contains("*"))
+                return "";
+            builder.append(phrase + "\\" + tag + " ");
+        }else{
+            for(int i=0; i<childs.size(); i++){
+                Phrase p = childs.get(i);
+                boolean space = (i != childs.size()-1);
+                builder.append(p.toString());
+            }
+        }
+        return builder.toString();
+    }
+}
---
+++ src/parser/CorpusParser.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parser;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import model.Phrase;
+
+/**
+ *
+ * @author macair
+ */
+public class CorpusParser {
+    
+    private static Phrase phrase;
+    private static BufferedReader br;
+    private static BufferedWriter bw ;
+    
+    public static void main(String[] args) throws Exception{
+        br = new BufferedReader(new FileReader("corpus tagged.txt"));
+        bw = new BufferedWriter(new FileWriter("corpus simple.txt"));
+        String line;
+        while((line = br.readLine()) != null){
+            line = br.readLine();
+//            System.out.println(line);
+            process(line, 0);
+        }
+        
+        bw.close();
+        br.close();
+    }
+    
+    public static void process(String str, int startIdx) throws Exception{
+        int idx1 = str.indexOf("(", startIdx) + 1;
+        int idx2 = str.indexOf(" ", idx1);
+        String tag = str.substring(idx1, idx2);
+        idx1 = idx2+1;
+        idx2 = getEndidx(str, idx1);
+        phrase = new Phrase(str.substring(idx1, idx2), tag, true);
+        String output = phrase.toString();
+        System.out.println(output);
+        bw.write(output + "\n");
+    }
+    
+    public static int getEndidx(String str, int curIdx){
+        int counter = 1;
+        int idx = curIdx;
+        while(counter > 0){
+            if(str.charAt(idx) == '(')
+                counter++;
+            else if(str.charAt(idx) == ')')
+                counter--;
+            idx++;
+        }
+        return idx-1;
+    }
+}
